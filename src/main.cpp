9c7bb2c83cab413362e4ac3c0d8dc50484578b79
Andika Naufal Ardana <andikana884@gmail.com>
2025-03-04 09:19:41 +0700

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+
+const int echoPin = 2;
+const int trigPin = 3;
+
+const int ledMerahPin = 6;
+const int ledHijauPin = 7;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(ledHijauPin, OUTPUT);
+  pinMode(ledMerahPin, OUTPUT);
+}
+
+// Membuat fungsi ultrasonik untuk mendapatkan jarak dari sensor ultrasonik
+float ultrasonik() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  int durasiPantulan = pulseIn(echoPin, HIGH);
+
+  // Membuat nilai dari fungsi ultrasonik menjadi hasil perhitungan durasi * 0.034 / 2
+  return durasiPantulan * 0.034 / 2;
+}
+
+void loop() {
+  float jarak = ultrasonik();
+
+  if (jarak >= 10) {
+    digitalWrite(ledHijauPin, HIGH);
+    digitalWrite(ledMerahPin, LOW);
+
+    Serial.println("Tidak ada objek");
+  } else {
+    digitalWrite(ledHijauPin, LOW);
+    digitalWrite(ledMerahPin, HIGH);
+
+    Serial.println("Ada objek");
+  }
+
+  delay(1000);
+}
+
